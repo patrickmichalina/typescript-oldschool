990358c55ea0392941352acd4162a2ffc4001004
Patrick Michalina <patrickmichalina@mac.com>
2018-10-04 14:25:14 -0500

    wip

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,20 @@ import * as express from 'express'
 import { homeModule } from './home/home.module'
 import { aboutModule } from './about/about.module'
 import { addToEngine } from './shared/util/add-to-engine'
+import { localsMiddleware } from './shared/middleware/locals'
 
 const app = express()
-const addModuleToOurApp = addToEngine(app)
 
+app.disable('x-powered-by')
 app.set('view engine', 'pug')
 app.set('views', 'src')
-app.disable('x-powered-by')
+
+app.use(express.static('.dist/.public'))
+app.use(localsMiddleware({
+  basedir: '.dist/.public'
+}))
+
+const addModuleToOurApp = addToEngine(app)
 
 addModuleToOurApp(homeModule)
 addModuleToOurApp(aboutModule)
