3af442a2c21ebc05ba472a466f17cf216cca64f7
Patrick Michalina <patrickmichalina@mac.com>
2018-10-06 12:04:36 -0500

    wip

--- src/shared/middleware/locals.ts
+++ src/shared/middleware/locals.ts
@@ -1,10 +1,12 @@
 import { Response, Request, NextFunction } from 'express'
 
-export const localsMiddleware = (options = {} as any) => (_req: Request, res: Response, next: NextFunction) => {
-  // tslint:disable-next-line:no-object-mutation
-  res.locals = {
-    ...res.locals,
-    ...options
-  }
-  next()
-}
+export const localsMiddleware =
+  (options = {} as any) =>
+    (_req: Request, res: Response, next: NextFunction) => {
+      // tslint:disable-next-line:no-object-mutation
+      res.locals = {
+        ...res.locals,
+        ...options
+      }
+      next()
+    }
