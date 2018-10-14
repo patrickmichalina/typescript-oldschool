65ecfb10414c10ff7e143890cbcd88e8b7d2975f
Patrick Michalina <patrickmichalina@mac.com>
2018-10-13 22:29:47 -0500

    update regex

--- tools/build/pwa.ts
+++ tools/build/pwa.ts
@@ -23,7 +23,7 @@ require('workbox-build')
         }
       },
       {
-        urlPattern: /\/(.+)\/([^\./]+)*$/,
+        urlPattern: /\/(.+)\/([^\./]+)*$/gm,
         handler: 'networkFirst',
         options: {
           cacheName: 'pages'
