a5c89aab7f86ca243b6a96694a8ee96cb84f83de
Patrick Michalina <patrickmichalina@mac.com>
2019-08-12 21:35:20 -0500

    pkgs

--- src/shared/components/card/card-component.script.ts
+++ src/shared/components/card/card-component.script.ts
@@ -3,7 +3,6 @@ import { fromEvent } from 'most'
 Array.from(document.querySelectorAll<HTMLDivElement>('.card-component')).forEach(element => {
   console.log('I am a component that needs javascript all to myself :)')
   fromEvent('click', element)
-    .take(1)
     .map(evt => (evt.target as HTMLElement).getAttribute('data-meta'))
     .observe(console.log)
 })
