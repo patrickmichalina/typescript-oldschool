af8153512284377f1fe159b8699a2b2d325be832
Patrick Michalina <patrickmichalina@mac.com>
2019-08-23 20:16:26 -0500

    fix

--- src/shared/components/card/card-component.script.ts
+++ src/shared/components/card/card-component.script.ts
@@ -3,6 +3,6 @@ import { fromEvent } from 'most'
 Array.from(document.querySelectorAll<HTMLDivElement>('.card-component')).forEach(element => {
   console.log('I am a component that needs javascript all to myself :)')
   fromEvent('click', element)
-    .map(evt => (evt.target as HTMLElement).getAttribute('data-meta'))
+    .map(_ => element.getAttribute('data-meta'))
     .observe(console.log)
 })
